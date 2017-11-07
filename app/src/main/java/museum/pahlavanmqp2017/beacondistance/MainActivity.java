da22941d569be913dcd29a85395e68401fef0afc
Ploob <32563170+Ploob@users.noreply.github.com>
2017-11-07 13:00:28 -0500

    Version 1.2 Initial Commit

--- app/src/main/java/museum/pahlavanmqp2017/beacondistance/MainActivity.java
+++ app/src/main/java/museum/pahlavanmqp2017/beacondistance/MainActivity.java
@@ -1,69 +1,128 @@
 package museum.pahlavanmqp2017.beacondistance;
 
+import android.app.Application;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 
-import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
-import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
-import com.estimote.coresdk.recognition.packets.Beacon;
-import com.estimote.coresdk.service.BeaconManager;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
 
-import java.util.List;
-import java.util.UUID;
+/*
+    This app runs as the top layer, sitting here until it sees a beacon
+    and moves to the specified class in didEnterRegion.
+    Basically, switch to the monitoring/ranging once a beacon is detected
+    or we move into the field of defined allowed BLE identifiers
 
-public class MainActivity extends AppCompatActivity {
+ */
 
-    private BeaconManager beaconManager;
-    private BeaconRegion region;
 
+public class MainActivity extends Application implements BootstrapNotifier{
+    private static final String TAG = "BeaconDistance";
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private boolean haveDetectedBeaconsSinceBoot = false;
+    private MonitoringActivity monitoringActivity = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate();
+        //setContentView(R.layout.activity_main);
+        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
 
-        beaconManager = new BeaconManager(this);
+        // The string given is the requirements for the BLE packet received
+        // At the moment it will look for all iBeacon packets, per m: 0-3
+        // i parameters can be used to filter by minor, major,, etc.
+        beaconManager.getBeaconParsers().clear();
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+            setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
-            @Override
-            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<Beacon> list) {
-                if(!list.isEmpty()){
-                    Beacon nearestBeacon = list.get(0);
-                    TextView beaconRSSI = (TextView)findViewById(R.id.nearestRSSI);
-                    beaconRSSI.setText(nearestBeacon.getRssi());
-                }
-            }
-        });
-
-        region = new BeaconRegion("ranged region",
-                    UUID.fromString("6EE4D6A9-DD8E-550E-FF81-783E445F9C5B"),
-                    34226, 26639);
+        // Constructing the class and holding its reference allow for low-power mode
+        // Sleep when not visible, wake when visible
+        // Waking the app
+        Region region = new Region("backgroundRegion", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+        // Sleeping
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
     }
 
     @Override
-    protected void onResume(){
-        super.onResume();
-        // Request permissions for needed actions, replaced possibly with own UI
-        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    public void didEnterRegion(Region arg0) {
+        // In this example, this class sends a notification to the user whenever a Beacon
+        // matching a Region (defined above) are first seen.
+        Log.d(TAG, "did enter region.");
+        if (!haveDetectedBeaconsSinceBoot) {
+            Log.d(TAG, "auto launching MainActivity");
 
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startRanging(region);
+            // The very first time since boot that we detect an beacon, we launch the
+            // MainActivity
+            Intent intent = new Intent(this, MonitoringActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
+            // to keep multiple copies of this activity from getting created if the user has
+            // already manually launched the app.
+            this.startActivity(intent);
+            haveDetectedBeaconsSinceBoot = true;
+        } else {
+            if (monitoringActivity != null) {
+                // If the Monitoring Activity is visible, we log info about the beacons we have
+                // seen on its display
+                monitoringActivity.logToDisplay("I see a beacon again");
+            } else {
+                // If we have already seen beacons before, but the monitoring activity is not in
+                // the foreground, we send a notification to the user on subsequent detections.
+                Log.d(TAG, "Sending notification.");
+                sendNotification();
             }
-        });
-
+        }
     }
 
     @Override
-    protected void onPause(){
-        beaconManager.stopRanging(region);
-
-        super.onPause();
+    public void didExitRegion(Region region) {
+        if (monitoringActivity != null) {
+            monitoringActivity.logToDisplay("I no longer see a beacon.");
+        }
     }
 
-    public void onClick_buttonBeginReadings(View view){
-
+    @Override
+    public void didDetermineStateForRegion(int state, Region region) {
+        if (monitoringActivity != null) {
+            monitoringActivity.logToDisplay("I have just switched from seeing/not seeing beacons: " + state);
+        }
     }
+
+    /*
+    private void sendNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("Beacon Reference Application")
+                        .setContentText("An beacon is nearby.")
+                        .setSmallIcon(R.drawable.ic_launcher);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(new Intent(this, MonitoringActivity.class));
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+
+    public void setMonitoringActivity(MonitoringActivity activity) {
+        this.monitoringActivity = activity;
+    }
+    */
 }
